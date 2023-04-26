ff7ad0d9e5d37bb4482849a101bf0a31b44d96c2
John McAlister <jmcalis6@vols.utk.edu>
2023-04-25 22:37:30 -0400

    Organized python files so that McAlisterProjectDemo could be written to showcase all work so far

--- GTDS_equilibria.py
+++ GTDS_equilibria.py
@@ -7,9 +7,61 @@ import matplotlib.colors as mcolors
 import random
 import orbit
 
+print("here")
+def do_this():
+    print("We start by generating a random connected graph")
+    n = 40 
+    p = 0.1
+    G=gen_connected_graph(n,p)
+    print("Then we can create an orbit object from this graph")
+    myOrbit = orbit.Orbit(G,[random.randint(0,40) for i in range(len(G.nodes))])
+    print(myOrbit.report)
+    cliques = find_cliques(myOrbit)
+    print("the orbit has a limit with {} cliques".format(len(cliques)))
+    myOrbit.animation()
+    
+
+
+#n=len(cliques)
+#myOrbit.animation()
+#limit = myOrbit.get_limit()
+#print(np.shape(np.asarray(myOrbit.get_limit())))
+#if myOrbit.eq:
+#   myOrbit.draw()
+#else:
+#    myOrbit.draw(2)
+#print(cliques)
+#print("This orbit has a limit with {} cliques".format(n))
+
+#clique_counts = count_cliques(100,0.02,100)
+#print(clique_counts)
+#print(np.arange(clique_counts.min(),clique_counts.max()+1))
+#plt.hist(clique_counts,bins = np.arange(clique_counts.min(),clique_counts.max()+1))
+#plt.show()
+
+
 
 def gen_connected_graph(n,p):
+    ''' 
+    function to generate a connected graph
+
+    Parameters
+    -----------
+    n : int
+        The number of vertices in the graph
+    p : float
+        The edge liklihood value for the Erdos Renyi graph generation
+
+    Returns 
+    ----------
+    G : nx.Graph
+        A conneceted graph
+    '''
+
+    #start by generating a random Erdos Renyi Graph
     G=nx.erdos_renyi_graph(n,p)
+
+    #get a list of connected components and connect them 
     comps=list(nx.connected_components(G))
     for ii in np.arange(0,len(comps)-1):
         G.add_edge(random.choice(list(comps[ii])),random.choice(list(comps[ii+1])))
@@ -17,6 +69,20 @@ def gen_connected_graph(n,p):
     return G
 
 def find_cliques(sol):
+    '''
+    A function to return the cliques of a equilibrium coloring
+
+    Parameters
+    ----------
+    sol : Orbit
+        The orbit to be analyzed
+    
+    Returns
+    -------
+    Q : list 
+        A list of networkx.Graph objects, each of which are a clique of the limit of sol.
+    '''
+    
     G=sol.graph
     lim = sol.get_limit()
     Q=list()
@@ -43,6 +109,7 @@ def find_cliques(sol):
         print("There is no equilibrium or 2-cycle in this orbit")
     return Q 
 
+#This function is a work in progress, I will not call it below
 def count_cliques(n,p,count):
     clique_count = np.zeros(count)
     for ii in np.arange(0,count):
@@ -50,16 +117,14 @@ def count_cliques(n,p,count):
         clique_count[ii] = len(cliques)
     return clique_count        
 
-#myOrbit = orbit.Orbit(gen_connected_graph(20,0.2))
-#G=nx.complete_bipartite_graph(4,2)
 
-
-G=gen_connected_graph(40,0.1)
-myOrbit = orbit.Orbit(G,[random.randint(0,40) for i in range(len(G.nodes))])
-cliques = find_cliques(myOrbit)
-print(myOrbit.report)
-n=len(cliques)
-myOrbit.animation()
+#main()
+#G=gen_connected_graph(40,0.1)
+#myOrbit = orbit.Orbit(G,[random.randint(0,40) for i in range(len(G.nodes))])
+##cliques = find_cliques(myOrbit)
+#print(myOrbit.report)
+#=len(cliques)
+#myOrbit.animation()
 #limit = myOrbit.get_limit()
 #print(np.shape(np.asarray(myOrbit.get_limit())))
 #if myOrbit.eq:
@@ -67,7 +132,7 @@ myOrbit.animation()
 #else:
 #    myOrbit.draw(2)
 #print(cliques)
-print("This orbit has a limit with {} cliques".format(n))
+#print("This orbit has a limit with {} cliques".format(n))
 
 #clique_counts = count_cliques(100,0.02,100)
 #print(clique_counts)
--- orbit.py
+++ orbit.py
@@ -6,7 +6,52 @@ import matplotlib as mpl
 import random 
 
 class Orbit:
+    '''A solution to the IVP for the GTDS
+
+
+    Attributes
+    ----------
+        graph : nx.Graph
+            the graph on which the IVP is solved 
+        solution : list 
+            a sequence of colorings, colors are represented as integers
+        iter : int
+            The length of the solution
+        eq : bool
+            True if the solution ends in an equilibrium
+        cycle2 : bool
+            True if the solution ends in a two-cycle
+        cycle3 : bool
+            True if the solution ends in a three-cycle
+        report : str
+            A string which describes the solution and its limit. 
+    Methods
+    ---------
+        draw(n=1, dim=None)
+            Shows a figure with the final n steps of orbit colored on the graph
+        get_limit()
+            Returns the last coloring in the orbit it the orbit ends in an equilibrium,
+            the last two colorings if the orbit ends in a two-cycle, and the last three
+            if the orbit ends in a three-cycle
+        animate()
+            Shows an animation of the orbit starting from the initial coloring
+    '''
+
     def __init__(self,G,y0=None,iter_limit=5000):
+        '''
+        Constructs all necessary attributes of the Orbit object
+        
+        Parameters
+        ----------
+        G : nx.Graph
+            A graph on which to solve the IVP
+        y0 : list, optional
+            A list of ints of the same length as the number of vertices in G
+            which describes the initial color of each vertex
+        iter_limit : int, optional
+            The upper iteration limit for the __solve()__ function. By default it is
+            set to 5000
+        '''
         self.graph = G
         result = self.__solve__(y0,iter_limit)
         self.solution = result[0]
@@ -17,46 +62,107 @@ class Orbit:
         self.report = result[5]
 
     def draw(self,n=1, dim=None):
+        '''
+        Draws the last n colorings of the orbit arranged as subplots
+
+        Parameters
+        ----------
+        n : int, optional
+            The number of colorings to be drawn. Given value 1 by default
+        dim : tuple, optional
+            The dimensions of the subplots as (rows,columns). By default it will be calculated
+            to be as close to square as possible
+        '''
+        #calculate dimensions
         if dim == None:
             r = int(np.floor(np.sqrt(n)))
             c = int(np.ceil(n/r))
         else:
             r=dim[0]
             c=dim[1]
+        
+        #get position so that each frame uses the same arrangement
         pos=nx.spring_layout(self.graph)
+
         plt.figure()
         for ii in np.arange(1,n+1):
             plt.subplot(r,c,ii)
             ec=nx.draw_networkx_edges(self.graph,pos)
-            nc = nx.draw_networkx_nodes(self.graph, pos, nodelist=self.graph.nodes(),node_color = self.solution[-(ii)],node_size = 20)
+            nc = nx.draw_networkx_nodes(self.graph, 
+                                        pos, 
+                                        nodelist=self.graph.nodes(),
+                                        node_color = self.solution[-(ii)],
+                                        node_size = 20)
         plt.show()
 
     def __str__(self):
+        '''
+        A method to return a description of the orbit as a string
+
+        Returns
+        --------
+        str
+        '''
         n=len(self.graph.nodes())
         st= "Starting from a given coloring of a connected graph on {} vertices, the orbit {}".format(n,self.report)
         return st 
 
     def __solve__(self,y0=None, iter_limit = 5000):
+        '''
+        A hidden method to find a solution to the IVP given initial data
+
+        parameters
+        ----------
+        y0 : list
+            A list of ints of the same length as the number of vertices in G
+            which describes the initial color of each vertex
+        iter_limit : int, optional
+            The maximum number of steps the solver will take while searching for a limit.
+            By default it is given the value 5000
+
+        Returns
+        -------
+            sol : list
+                the sequence of colorings resulting from the initial data and the update rule
+            iter : int
+                The number of iterations it took to reach a limit
+            eq : bool
+                True if the solution reached an equilibrium
+            cycle2 : bool
+                True if the solution reached a 2 cycle
+            cycle3 : bool 
+                True if the solution reached a 3 cycle
+            report : str
+                A string describing the solution 
+        '''
+
+        #if no initial data is given, give each vertex a different color
         if y0 is None:
             y0 = np.arange(0,len(self.graph.nodes()))
+        
+        # start solution with initial data
         iter = 0
-        orbit=list()
-        orbit.append(y0)
-        yold=np.asarray((-1,)*len(self.graph.nodes()))
+        sol=list()
+        sol.append(y0)
+        
+        #prepare to check termination conditions
         finished = False
         eq=False
         cycle2 = False
         cycle3=False
+
         while not finished:
-            yold=orbit[-1]
-            orbit.append(self.__F__(yold).astype(int))
+            #Add a new coloring according to the update rule
+            sol.append(self.__F__(sol[-1]).astype(int))
             iter=iter+1 
+            #Check to see if the solution has reached a limit
             if iter>=19:
-                eq = (orbit[-1]==orbit[-2]).all() and (orbit[-1]==orbit[-3]).all()
-                cycle2 = (orbit[-1]==orbit[-3]).all() and (orbit[-1]==orbit[-5]).all() and (orbit[-1]==orbit[-19]).all()
-                cycle3 = (orbit[-1]==orbit[-4]).all() and (orbit[-1]==orbit[-7]).all() and (orbit[-1]==orbit[-19]).all()
+                eq = (sol[-1]==sol[-2]).all() and (sol[-1]==sol[-3]).all()
+                cycle2 = (sol[-1]==sol[-3]).all() and (sol[-1]==sol[-5]).all() and (sol[-1]==sol[-19]).all()
+                cycle3 = (sol[-1]==sol[-4]).all() and (sol[-1]==sol[-7]).all() and (sol[-1]==sol[-19]).all()
             finished = iter>=iter_limit or eq or cycle2 or cycle3
 
+        # Once the solution has reached a limit, build the report 
         if eq:
             str="The orbit ended at an equilibrium"
         elif cycle2:
@@ -66,12 +172,30 @@ class Orbit:
         else: 
             str= "The orbit not result in an equilibrium or small limit cycle. A limit may exist, try again with a larger iter_limit"
         report = "terminated in {} iterations. {}".format(iter, str)
-        return [orbit,iter, eq, cycle2, cycle3,report]
+
+        #return a list of 6 different objects
+        return [sol,iter, eq, cycle2, cycle3,report]
     
     def __F__(self,y):
+        '''
+        A hidden method for generating the next coloring given the current coloring
+
+        Parameters
+        ----------
+        y : np.array 
+            The current coloring of the graph
+        
+        Returns
+        -------
+        ynew : np.array
+            The vector of best responses to y
+        '''
         ynew = np.zeros(len(y))
         for n in np.arange(0,len(y)):
+            #get the set of best responses for vertex n
             argmax = self.__mode__([y[i]for i in list(self.graph.neighbors(n))])
+
+            #break ties according to the tie breaking protocol 
             if y[n] in argmax:
                 ynew[n]=y[n]
             else:
@@ -79,10 +203,30 @@ class Orbit:
         return ynew
 
     def __mode__(self,array):
+        '''
+        A hidden method for finding the mode of an array
+
+        Parameters
+        ----------
+        array : iterable
+        
+        Returns
+        --------
+        mode : list
+            a list of objects which were present the greatest number of times in array
+        '''
         most = max(list(map(array.count, array)))
         return list(set(filter(lambda x: array.count(x) == most, array)))
     
     def get_limit(self):
+        '''
+        A method to retreive the limit of an orbit
+
+        Returns
+        --------
+        limit : list
+            The colorings which together make up the limit of the orbit
+        '''
         if self.eq:
             return self.solution[-1]
         elif self.cycle2:
@@ -94,6 +238,28 @@ class Orbit:
             return self.solution[-5:]
     
     def __animate__(self,framenumber, pos,cmap):
+        '''
+        A hidden method to update the figure in the animate() method
+
+        Parameters
+        ----------
+        framenumber : int
+
+        pos : artist
+            A parameter used to determine the position of the edges and nodes in the frame
+        cmap : colormap
+            A colormap which associates colors as described by self.soltion to RGB colors.
+
+        Returns
+        --------
+        ec : Artist 
+            The artist object which draws the edges of the graph
+        nc : Artist
+            The artist object which draws the nodes of the graph
+        title : Artist
+            The artist object which draws the title of the graph
+
+        '''
         cols = self.__getcolors__(cmap,framenumber)
         ec=nx.draw_networkx_edges(self.graph,pos)
         nc=nx.draw_networkx_nodes(self.graph, pos, 
@@ -104,13 +270,21 @@ class Orbit:
         return(ec,nc,title)
 
     def animation(self):
+        '''
+        Method to show an animation of the Orbit, starting from the intial condition
+        and ending at the limit
+        '''
+
         #Start by assigning colors to stratagies
         strats = list(set(self.solution[0]))
         stmax= max(strats)
         hsv = mpl.colormaps['hsv']
         cmap = hsv(np.linspace(0,hsv.N,stmax+1).astype(int))
 
+        #calls __getcolors__ to get a list of colors from the strategies
         cols = self.__getcolors__(cmap,0)
+
+        #make a position object so each frame has the graph in the same layout
         pos=nx.spring_layout(self.graph)
         fig = plt.figure()
         title =plt.title('$t=0$')
@@ -125,8 +299,26 @@ class Orbit:
                                         fargs = (pos,cmap),
                                         blit = True)
         plt.show()
+        return ani 
 
     def __getcolors__(self,cmap,frame):
+        ''' 
+        A hidden method to make a list of RGB from a list step in the soluiton
+
+        Parameters
+        ---------
+        cmap : colormap
+            A colormap associating colors in the solution to actual RGB colors
+        frame : int
+            The frame number
+
+        Returns
+        ---------
+        cols : np.array
+            A list with the same length as the number of vertices in the graph which associates
+            a vertex to an RGB color by the color given in self.solution[frame]
+        '''
+
         y = self.solution[frame]
         n=len(y)
         cols = np.zeros((n,4))
