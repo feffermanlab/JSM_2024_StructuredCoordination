81c4358a6ff958c1dcc112865b929e88b4ef9b04
John McAlister <jmcalis6@vols.utk.edu>
2024-01-11 15:25:10 -0500

    Completed the script to find every equilibrium partition on small connected graphs
    produced an output file but there is still work to be done to organize the results

--- SCCatalogue.py
+++ SCCatalogue.py
@@ -14,37 +14,64 @@ import orbit
 
 
 def main(start,end):
-    Atlas = nx.graph_atlas_g()[start:end]#
+    Atlas = nx.graph_atlas_g()[start:end]
+    #order 2: 3
+    #order 3: [4,5]
+    #order 4: [6,11]
+    #order 5: [26,52]
+    #order 6: [75, 208]
+
     df = pd.DataFrame({"name":[],"n":[], "partition":[], "ClusterNumber":[]})
+    configList = [[0],genConfigs(1),genConfigs(2),genConfigs(3),genConfigs(4),genConfigs(5),genConfigs(6)]
     graphnumber = start
     index = 0
     for G in Atlas:
         if(nx.is_connected(G)):
             print(graphnumber)
             n = G.number_of_nodes()
-            partitions = genConfigs(n)
+            partitions = configList[n]
             for p in partitions:
                 o = orbit.Orbit(G,p,2)
                 if (o.solution[0]==o.solution[1]).all():
                     df.loc[index]=(graphnumber, n, p, len(set(p)))
                     index = index +1
-            graphnumber= graphnumber +1
+                    if len(set(p))>1:o.draw()
+                    #if len(set(p))==1:o.draw()
+        graphnumber= graphnumber +1
+        #This cannot yet differentiate between partitions which are identical on the basis of graphical symetry. 
+    decomposable =df[df['ClusterNumber']>1]
+    decomposable.to_excel("Decomposable6.xlsx", sheet_name ="decomposable6", index = False)
+    decomposable = list(set(decomposable['name']))
+    print(decomposable)
+    #for n in range(start,end):
+    #   print(n)
+    #    if not(n in decomposable): 
+    #        print("true")
+    #        nx.draw(Atlas[n-start])
+    #        plt.show()
     return df
 
 
+'''generates all the non-isomorphic partitions on n vertices with floor(n/2) parts'''
 def genConfigs(n):
     fl = np.floor(n/2) 
-    configs = list()
-    m = fl**(n-1)
+    configs = list() #prepares an empty list to hold partitions
+    m = fl**(n-1) # the total number of partitions to be made
     for i in np.arange(0,m):
-        digits = numberToBase(i, fl)
-        x=canonicalConfig(list(np.zeros(n-len(digits),dtype = int))+digits)
-        if not any( (c == x).all() for c in configs):
+        digits = numberToBase(i, fl) #converts current partition number into base fl
+        #Ensure that the length of the partition is correct
+        #Turn the partitions into "cannonical forms" so isomorphic duplicates are not added
+        x=canonicalConfig(list(np.zeros(n-len(digits),dtype = int))+digits) 
+        if not any( (c == x).all() for c in configs): #If it's not already there, add it.
             configs.append(x)
+
+            #Here there is an improvement to be made. We should only add those partitions which do not
+            #Parts of size 1. How can we test for this easily?
+
     return(configs)
 
 
-
+''' Convert a number n into base b'''
 def numberToBase(n, b):
     if n == 0:
         return [0]
@@ -54,6 +81,8 @@ def numberToBase(n, b):
         n //= b
     return digits[::-1]
 
+
+'''take a partition and return the canonical isomorphic partition'''
 def canonicalConfig(config):
     l = config
     cl = np.zeros(len(l),dtype= int)
@@ -65,4 +94,4 @@ def canonicalConfig(config):
     return(cl)
 
 
-print(main(29,50))
+print(main(75,209))
